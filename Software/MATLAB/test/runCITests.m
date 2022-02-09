cb2966ba1a24fc0dc734b5c2104a8af46e6616e9
Martijn Aben <maben@mathworks.com>
2022-02-09 10:55:16 +0100

    One final test report

--- Software/MATLAB/test/runCITests.m
+++ Software/MATLAB/test/runCITests.m
@@ -1,24 +1,25 @@
-import matlab.unittest.TestRunner;
-import matlab.unittest.Verbosity;
-import matlab.unittest.plugins.XMLPlugin;
-import matlab.unittest.selectors.HasName;
-import matlab.unittest.constraints.StartsWithSubstring;
-  
-% Generate a suite with all tests
-suite = matlab.unittest.TestSuite.fromFolder('../../../Modules/matlab-spark-api/Software/MATLAB/test/unit');
+function runCITests(name)
+    import matlab.unittest.TestRunner;
+    import matlab.unittest.Verbosity;
+    import matlab.unittest.plugins.XMLPlugin;
+    import matlab.unittest.selectors.HasName;
+    import matlab.unittest.constraints.StartsWithSubstring;
+    
+    % Generate a suite with all tests
+    suite = matlab.unittest.TestSuite.fromFolder('../../../Modules/matlab-spark-api/Software/MATLAB/test/unit');
 
-% Filter out tests which require a MATLAB Compiler SDK license
-suite = suite.selectIf(~HasName(StartsWithSubstring('testSparkBuilder')));
+    % Filter out tests which require a MATLAB Compiler SDK license
+    suite = suite.selectIf(~HasName(StartsWithSubstring('testSparkBuilder')));
 
-% Configure for JUnit XML reporting
-[~,~] = mkdir('../../../test-results');
+    % Configure for JUnit XML reporting
+    [~,~] = mkdir('../../../test-results');
 
-runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
-runner.addPlugin(XMLPlugin.producingJUnitFormat('../../../test-results/results.xml'));
+    runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
+    runner.addPlugin(XMLPlugin.producingJUnitFormat(['../../../test-results/' name '.xml']));
 
-% Run the suite
-results = runner.run(suite);
+    % Run the suite
+    results = runner.run(suite);
 
-% Assert that none of the tests failed
-nfailed = nnz([results.Failed]);
-assert(nfailed == 0, [num2str(nfailed) ' test(s) failed.']);
+    % Assert that none of the tests failed
+    nfailed = nnz([results.Failed]);
+    assert(nfailed == 0, [num2str(nfailed) ' test(s) failed.']);
